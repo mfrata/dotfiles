8245fa345d997707d85de36dd8a3fc2488e8142d
Matheus Frata <matheus.frata@neoway.com.br>
2021-01-03 22:24:26 -0300

    Add create album from dir of photos

--- bin/photo/lib/photo-related-variables.sh
+++ bin/photo/lib/photo-related-variables.sh
@@ -1,3 +1,4 @@
 TODO_DIR=~/frata/0-todo/photos
 ARCHIVE_DIR=~/frata/2-personal/images/photos
+ALBUNS_DIR=~/frata/2-personal/images/albuns
 CANON_SL3_MOUNT_FOLDER=/media/matheus-frata/6431-3438/DCIM/100CANON
