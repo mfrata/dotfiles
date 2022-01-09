8ed33875432096daf3f01b5ebd10864c54684ac2
Matheus Frata <matheus.frata@neoway.com.br>
2022-01-09 19:00:34 +0100

    feat(nvim): add dashboard

---
+++ config/nvim/lua/user/dashboard.lua
@@ -0,0 +1,22 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.buttons.val = {
+	dashboard.button("n", "New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "Planning", ":e ~/Documents/self/planning/week.md <CR>"),
+	dashboard.button("c", "Configuration", ":e ~/.dotfiles/config/nvim <CR>"),
+	dashboard.button("q", "Quit Neovim", ":qa<CR>"),
+}
+
+local function footer()
+	return "mfrata"
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -117,6 +117,8 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
+  use {'goolord/alpha-nvim', requires = { 'kyazdani42/nvim-web-devicons' }}
+
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
