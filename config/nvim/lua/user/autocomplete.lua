a86f13001a4e96ad3a646c738ef68ad3c1132896
Matheus Frata <git@mfrata.com>
2022-05-01 11:06:25 +0200

    Fix: nvim-cmp error on doc

--- config/nvim/lua/user/autocomplete.lua
+++ config/nvim/lua/user/autocomplete.lua
@@ -121,8 +121,8 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = "native"
   },
   experimental = {
     ghost_text = false,
