3f21be51b63a3d5c2d094f13cf27367402eb31bf
Matheus Frata <git@mfrata.com>
2023-02-05 18:09:00 +0100

    refactor(nvim.init): require is function now

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,1 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.autocomplete"
-require "user.lsp"
-require "user.dashboard"
+require("user")
---
+++ config/nvim/lua/user/init.lua
@@ -0,0 +1,7 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.autocomplete"
+require "user.lsp"
+require "user.dashboard"
